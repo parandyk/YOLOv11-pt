c1614c4b7bfec6fb7954ee9880e823324117c25c
parandyk <mparandyk@interia.pl>
2025-06-03 23:56:13 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
 GPUS=$1
-python3 -m torch.distributed.launch --nproc_per_node=$GPUS /kaggle/working/main.py ${@:2}
+python3 -m torch.distributed.launch --nproc_per_node=$GPUS /kaggle/working/YOLOv11-pt/main.py ${@:2}
