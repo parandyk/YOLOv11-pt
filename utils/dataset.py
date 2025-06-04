1cea5f12e77a4292476562ad5deb3f58ab11239b
parandyk <mparandyk@interia.pl>
2025-06-04 14:41:18 +0200

    Update dataset.py
    collate fn update

--- utils/dataset.py
+++ utils/dataset.py
@@ -187,10 +187,10 @@ class Dataset(data.Dataset):
     
     @staticmethod
     def collate_fn(batch):
-    if torch.cuda.is_available():
-        return CustomBatches(batch)
-    else:
-        return tuple(zip(*batch))
+        if torch.cuda.is_available():
+            return CustomBatches(batch)
+        else:
+            return tuple(zip(*batch))
     # def collate_fn(batch):
     #     new_batch = {}
     #     keys = batch[0].keys()
