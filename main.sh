c2427965567e75f61b84ac2152cc553501118cb5
parandyk <mparandyk@interia.pl>
2025-06-05 10:12:59 +0200

    Update main.sh
    debugging cuda due to: RuntimeError: CUDA error: device-side assert triggered

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 GPUS=$1
 cd $2
 pwd
-python3 -m torch.distributed.launch --nproc_per_node=$GPUS main.py ${@:3}
+CUDA_LAUNCH_BLOCKING=1 python3 -m torch.distributed.launch --nproc_per_node=$GPUS main.py ${@:3}
