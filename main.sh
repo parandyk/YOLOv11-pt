df7e8b098aedb83254fabd85747a1aac30ce622a
parandyk <mparandyk@interia.pl>
2025-06-06 11:53:13 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
 GPUS=$1
 cd $2
 pwd
-CUDA_LAUNCH_BLOCKING=1 python3 -m torch.distributed.launch --nproc_per_node=$GPUS main.py ${@:3}
+#CUDA_LAUNCH_BLOCKING=1 
+python3 -m torch.distributed.launch --nproc_per_node=$GPUS main.py ${@:3}
