fe46e1ea3d64698f2a28db5b4e958806e2778a6c
parandyk <mparandyk@interia.pl>
2025-06-06 14:18:34 +0200

    Update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -251,10 +251,10 @@ class Dataset(data.Dataset):
             for key in ("labels", "boxes"):
                 target[key] = list(map(lambda t: t if isinstance(t, torch.Tensor) else torch.tensor([]), targets[key]))
                 target[key] = torch.cat(target[key], dim=0)
-
-            print(f'to target labele: {target["labels"]}')
-            target["idx"] = torch.tensor([list(map(lambda t: torch.arange(t.size(0)) if isinstance(t, torch.Tensor) else torch.tensor([]), target["labels"]))]) #if len(t.shape) > 0 else #? this or below? 
+                
+            #target["idx"] = torch.tensor([list(map(lambda t: torch.arange(t.size(0)) if isinstance(t, torch.Tensor) else torch.tensor([]), target["labels"]))]) #if len(t.shape) > 0 else #? this or below? 
             #target["idx"] = torch.tensor([list(map(lambda t: t if isinstance(t, torch.Tensor) else torch.tensor([]), target["labels"]))]) # or torch.cat(list) or torch.tensor([list])
+            target["idx"] = torch.arange(target["labels"].size(0))
             target["cls"] = target.pop("labels")
             target["box"] = target.pop("boxes")
         
